a4d7c4afc86f1b7c28da6257796206e7ac3a795c
Rocky Li <aperocky@gmail.com>
2019-12-12 00:35:27 -0500

    Add readme to pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
+with open("readme.md", encoding='utf-8') as f:
+    readme = f.read()
+
 setup(name='sqlitedao',
-      version='0.1.0',
+      version='0.1.1',
       description='simple single file dao for sqlite',
       author='Rocky Li',
       url='https://github.com/Aperocky/sqlitedao',
       author_email='aperocky@gmail.com',
       license='MIT',
+      long_description=readme,
+      long_description_content_type='text/markdown',
       packages=['sqlitedao'])
